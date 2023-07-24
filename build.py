dcc4459a4aca2f22ff23916911ac18fd2a006657
Matthew Taylor <matthew.taylor@ditchwitch.com>
2023-07-24 09:38:13 -0500

    Attempt to use workaround from poetry issue #6197

--- build.py
+++ build.py
@@ -1,13 +1,31 @@
 from typing import Any
 from grpc_tools import protoc
+from setuptools.command.build_py import build_py
 
-def build(setup_kwargs: dict[str, Any]):
-    
-    protoc_args = [
-        "-I=protocol",
-	    "--python_out=.",
-        "--grpc_python_out=.",
-        "protocol/example.proto",
-    ]
+# Adapted from https://github.com/python-poetry/poetry/issues/6197#issuecomment-1336221319
 
-    protoc.main(protoc_args)
+class BuildPyCommand(build_py):
+    def finalize_options(self):
+        super().finalize_options()
+        self.build_lib = f'{self.build_lib}.blah'
+
+    def run(self):
+        protoc_args = [
+            "-I=protocol",
+	        "--python_out=.",
+            "--grpc_python_out=.",
+            "protocol/example.proto",
+        ]
+
+        protoc.main(protoc_args)
+        
+        return super().run()
+
+def build(setup_kwargs):
+    setup_kwargs.update(
+        {
+            "cmdclass": {
+                "build_py": BuildPyCommand,
+            },
+        }
+    )
